daadbc9b8fe1125d40a26508e63486226af1a1e3
Dominic <Dominic@DOMINICSPC>
2021-09-29 19:51:51 -0600

    Created solution for practice code

---
+++ PracticeCode/PracticeCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
